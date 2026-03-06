503da5e72cfe43107878c6cc3e4685e31f697f24
David Holroyd <dave@badgers-in-foil.co.uk>
2026-03-06 16:01:43 +0000

    fix clippy lints

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -10,18 +10,18 @@ struct NullConsumer {}
 impl AdtsConsumer for NullConsumer {
     fn new_config(
         &mut self,
-        mpeg_version: MpegVersion,
-        protection: ProtectionIndicator,
-        aot: AudioObjectType,
-        freq: SamplingFrequencyIndex,
-        private_bit: u8,
-        channels: ChannelConfiguration,
-        originality: Originality,
-        home: u8,
+        _mpeg_version: MpegVersion,
+        _protection: ProtectionIndicator,
+        _aot: AudioObjectType,
+        _freq: SamplingFrequencyIndex,
+        _private_bit: u8,
+        _channels: ChannelConfiguration,
+        _originality: Originality,
+        _home: u8,
     ) {
     }
-    fn payload(&mut self, buffer_fullness: BufferFullness, number_of_blocks: u8, buf: &[u8]) {}
-    fn error(&mut self, err: AdtsParseError) {}
+    fn payload(&mut self, _buffer_fullness: BufferFullness, _number_of_blocks: u8, _buf: &[u8]) {}
+    fn error(&mut self, _err: AdtsParseError) {}
 }
 
 fuzz_target!(|data: &[u8]| {
